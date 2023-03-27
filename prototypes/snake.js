131141fee00a244614f9c24e4e0b9008be91c7f4
shohan-pherones <shohan.sub.56@gmail.com>
2023-03-28 05:13:04 +0600

    snake decision obtained

--- prototypes/snake.js
+++ prototypes/snake.js
@@ -37,4 +37,133 @@ class Snake {
   dispose() {
     this.brain.dispose();
   }
+
+  initSnakeBody() {
+    this.body.push(
+      new SnakeCell(
+        this.head.col - 1,
+        this.head.row,
+        this.head.size,
+        this.color
+      )
+    );
+    this.body.push(
+      new SnakeCell(
+        this.head.col - 2,
+        this.head.row,
+        this.head.size,
+        this.color
+      )
+    );
+    this.body.push(
+      new SnakeCell(
+        this.head.col - 3,
+        this.head.row,
+        this.head.size,
+        this.color
+      )
+    );
+    this.body.push(
+      new SnakeCell(
+        this.head.col - 4,
+        this.head.row,
+        this.head.size,
+        this.color
+      )
+    );
+  }
+
+  canvasDiagonal() {
+    return Math.abs(
+      Math.sqrt(
+        (canvas_height / BLOCK_SIZE) * (canvas_height / BLOCK_SIZE) +
+          (canvas_width / BLOCK_SIZE) * (canvas_width / BLOCK_SIZE)
+      )
+    );
+  }
+
+  think(apple) {
+    let input = [
+      this.checkFreeRight(),
+      this.checkFreeLeft(),
+      this.checkFreeInfront(),
+      this.appleDagree(apple),
+      this.decision(apple) / 10,
+    ];
+
+    let output = this.brain.predict(input);
+    this.choice(output, apple);
+  }
+
+  appleDagree(apple) {
+    if (this.decision == UP || this.decision == DOWN) {
+      if (this.head.row == apple.row) {
+        return map(Math.PI / 2, -Math.PI / 2, Math.PI / 2, -1, 1);
+      }
+
+      return map(
+        Math.atan((this.head.col - apple.col) / (this.head.row - apple.row)),
+        -Math.PI / 2,
+        Math.PI / 2,
+        -1,
+        1
+      );
+    }
+
+    if (this.head.col == apple.col) {
+      return map(Math.PI / 2, -Math.PI / 2, Math.PI / 2, -1, 1);
+    }
+
+    return map(
+      Math.atan(
+        Math.abs(this.head.row - apple.row) /
+          Math.abs(this.head.col - apple.col)
+      ),
+      -Math.PI / 2,
+      Math.PI / 2,
+      -1,
+      1
+    );
+  }
+
+  decision(apple) {
+    switch (this.direction) {
+      case UP:
+        if (this.head.col > apple.col) {
+          return TURN_LEFT;
+        } else if (this.head.col < apple.col) {
+          return TURN_RIGHT;
+        }
+
+        return DEFUALT;
+      case RIGHT:
+        if (this.head.row < apple.row) {
+          return TURN_RIGHT;
+        } else if (this.head.row > apple.row) {
+          return TURN_LEFT;
+        }
+
+        return DEFUALT;
+      case DOWN:
+        if (this.head.col < apple.col) {
+          return TURN_LEFT;
+        } else if (this.head.col > apple.col) {
+          return TURN_RIGHT;
+        }
+
+        return DEFUALT;
+      case LEFT:
+        if (this.head.row < apple.row) {
+          return TURN_LEFT;
+        } else if (this.head.row > apple.row) {
+          return TURN_RIGHT;
+        }
+
+        return DEFUALT;
+    }
+  }
+
+  pitagoras_third_vertex(a, b) {
+    return Math.sqrt(a * a + b * b);
+  }
 }
