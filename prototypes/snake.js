9ee8738fa4081ae309855c2137c6d7367a9e70ac
shohan-pherones <shohan.sub.56@gmail.com>
2023-03-28 12:45:53 +0600

    checking free in up

--- prototypes/snake.js
+++ prototypes/snake.js
@@ -166,4 +166,40 @@ class Snake {
   pitagoras_third_vertex(a, b) {
     return Math.sqrt(a * a + b * b);
   }
+
+  checkFreeInfront() {
+    switch (this.direction) {
+      case UP:
+        return this.head.row - 1 < 0 ||
+          this.body.find(
+            (cell) => cell.col == this.head.col && cell.row == this.head.row - 1
+          ) != undefined
+          ? 1
+          : 0;
+
+      case RIGHT:
+        return this.head.col + 1 >= canvas_width / BLOCK_SIZE ||
+          this.body.find(
+            (cell) => cell.col == this.head.col + 1 && cell.row == this.head.row
+          ) != undefined
+          ? 1
+          : 0;
+
+      case DOWN:
+        return this.head.row + 1 >= canvas_height / BLOCK_SIZE ||
+          this.body.find(
+            (cell) => cell.col == this.head.col && cell.row == this.head.row + 1
+          ) != undefined
+          ? 1
+          : 0;
+
+      case LEFT:
+        return this.head.col - 1 < 0 ||
+          this.body.find(
+            (cell) => cell.col == this.head.col - 1 && cell.row == this.head.row
+          ) != undefined
+          ? 1
+          : 0;
+    }
+  }
 }
